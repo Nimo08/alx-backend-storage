15336b0d971da85ac4c0f14134c2a6c138ff393c
Nimo08 <nimomohamed800@gmail.com>
2024-02-21 18:31:56 +0300

    Correcting lifespan calculation

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,11 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
+-- Calculate lifespan: if split is null replace it with 2022
+
 SELECT
     band_name,
-    DATEDIFF('2022-01-01', formed) AS lifespan
+    (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE
-    style = 'Glam rock'
+    style LIKE '%Glam rock%'
 ORDER BY
     lifespan DESC;
