b1e7c8fe7ec9ec44c5fe1b069f46396febeaedbe
Nimo08 <nimomohamed800@gmail.com>
2024-02-21 18:12:21 +0300

    Retrying task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,12 +5,20 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser( IN user_id INT)
 BEGIN
+    DECLARE total_score FLOAT;
+    DECLARE total_projects INT;
     DECLARE average_score FLOAT;
     
-    SELECT AVG(score) INTO average_score
+    SELECT SUM(score) , COUNT(project_id)
+    INTO total_score, total_projects
     FROM corrections
     WHERE user_id = user_id;
 
+    IF total_projects > 0 THEN
+        SET average_score = total_score / total_projects;
+    ELSE
+        SET average_score = 0;
+    END IF;
 END;
 //
 
