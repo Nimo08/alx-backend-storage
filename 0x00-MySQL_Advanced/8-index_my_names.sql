45d8a33912ce8c2717ef96ca14c1f49ce1b42f81
Nimo08 <nimomohamed800@gmail.com>
2024-02-21 18:16:22 +0300

    Specifying index using LEFT

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- Creates an index idx_name_first on the table names and the first letter of name.
 
-CREATE INDEX idx_name_first ON names (first_letter);
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
