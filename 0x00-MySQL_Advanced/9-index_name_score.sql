05b2de4b0ad4f9e254680afd7bb264bd2b98e7c7
Nimo08 <nimomohamed800@gmail.com>
2024-02-21 18:20:56 +0300

    Correcting index of name and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,8 +1,4 @@
 -- Creates an index idx_name_first_score on the table names and
 -- the first letter of name and the score.
 
-ALTER TABLE names
-ADD first_char CHAR(1) AS (LEFT(name, 1)),
-ADD first_num CHAR(1) AS (LEFT(CAST(score AS CHAR), 1));
-
-CREATE INDEX idx_name_first_score ON names(first_char, first_num);
+CREATE INDEX idx_name_first_score ON names(name(1), score);
