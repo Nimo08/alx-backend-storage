bd9896cdb23810d9d16a6e1df2e6c94bd5739b41
Nimo08 <nimomohamed800@gmail.com>
2024-02-21 19:12:32 +0300

    Checking task 13

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,19 @@
+-- Creates a stored procedure ComputeAverageWeightedScoreForUsers
+-- that computes and store the average weighted score for all students.
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+DETERMINISTIC
+BEGIN
+   
+    UPDATE users
+    SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+                            FROM corrections
+                            JOIN projects ON projects.id = corrections.project_id
+                            WHERE corrections.user_id = users.id);
+
+END;
+//
+
+DELIMITER ;
