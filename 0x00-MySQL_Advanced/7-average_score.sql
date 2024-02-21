6c33faaa5b47d3430ee34b363a8644c634fa8981
Nimo08 <nimomohamed800@gmail.com>
2024-02-21 18:46:10 +0300

    Computing average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,20 +5,15 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser( IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
-    DECLARE average_score FLOAT;
     
-    SELECT SUM(score) , COUNT(project_id)
-    INTO total_score, total_projects
+    SELECT AVG(score) AS avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
 
-    IF total_projects > 0 THEN
-        SET average_score = total_score / total_projects;
-    ELSE
-        SET average_score = 0;
-    END IF;
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id
+
 END;
 //
 
