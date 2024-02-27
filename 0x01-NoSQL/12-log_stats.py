a0fd42fff2634de505f59da921ce5f13e655cd06
Nimo08 <nimomohamed800@gmail.com>
2024-02-27 11:22:09 +0300

    Modifying script

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,13 +7,12 @@ Provides some stats about Nginx logs stored in MongoDB
 from pymongo import MongoClient
 
 
-client = MongoClient("localhost", 27017)
-db = client.logs
-collection = db.nginx
-
-total_logs = collection.nginx.count_documents({})
-
 if __name__ == "__main__":
+    client = MongoClient("localhost", 27017)
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.nginx.count_documents({})
     method_counts = {
         "GET": collection.count_documents({"method": "GET"}),
         "POST": collection.count_documents({"method": "POST"}),
@@ -28,5 +27,3 @@ if __name__ == "__main__":
     for method, count in method_counts.items():
         print(f"\tmethod {method}: {count}")
     print(f"{status_check_count} status check")
-
-client.close()
