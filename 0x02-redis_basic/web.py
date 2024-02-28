03cd1448af465226ad5a37bb262aa1e974d05cff
Nimo08 <nimomohamed800@gmail.com>
2024-02-28 13:52:01 +0300

    Setex in get_page

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """
-Implementing an expiring web cache and tracker
 """
 
 import redis
@@ -10,34 +9,37 @@ from functools import wraps
 redis_instance = redis.Redis()
 
 
-def count_url(func: Callable) -> Callable:
+def count(func: Callable) -> Callable:
     """
     Track how many times a particular URL was accessed
     in the key "count:{url}" and cache the result with an
     expiration time of 10 seconds
     """
     @wraps(func)
-    def wrapper(*args, **kwargs) -> Any:
+    def wrapper(self, *args, **kwargs) -> Any:
         url = args[0]
         count_key = f"count:{url}"
         redis_instance.incr(count_key)
-        cached_response = redis_instance.get(url)
-        if cached_response:
-            return cached_response.decode('utf-8')
-        redis_instance.setex(url, 10, func(url))
-        return func(*args, **kwargs)
+        return func(self, *args, **kwargs)
     return wrapper
 
 
-@count_url
 def get_page(url: str) -> str:
     """
     Uses the requests module to obtain the HTML content
     of a particular URL and returns it.
     """
+    cached_response = redis_instance.get(url)
+    if cached_response:
+        return cached_response.decode('utf-8')
     response = requests.get(url)
-    return response.text
+    html_content = response.text
+
+    redis_instance.setex(url, 10, html_content)
+    return html_content
 
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    url = "http://slowwly.robertomurray.co.uk"
+    html_content = get_page(url)
+    print(html_content)
