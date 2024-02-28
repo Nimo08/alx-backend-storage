472d00be4017d157433151a8cf2ad89cc8c63daa
Nimo08 <nimomohamed800@gmail.com>
2024-02-28 16:29:31 +0300

    Reverted to decorator & returning func(url)

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,23 +7,36 @@ import redis
 import requests
 from typing import Callable, Any
 from functools import wraps
+redis_instance = redis.Redis()
 
 
+def count_url(func: Callable) -> Callable:
+    """
+    Track how many times a particular URL was accessed
+    in the key "count:{url}" and cache the result with an
+    expiration time of 10 seconds
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        url = args[0]
+        count_key = f"count:{url}"
+        redis_instance.incr(count_key)
+        cached_response = redis_instance.get(url)
+        if cached_response:
+            return cached_response.decode('utf-8')
+        redis_instance.setex(url, 10, func(url))
+        return func(url)
+    return wrapper
+
+
+@count_url
 def get_page(url: str) -> str:
     """
     Uses the requests module to obtain the HTML content
     of a particular URL and returns it.
     """
-    redis_instance = redis.Redis()
-    count_key = f"count:{url}"
-    redis_instance.incr(count_key)
-    cached_response = redis_instance.get(url)
-    if cached_response:
-        return cached_response.decode()
     response = requests.get(url)
-    html_content = response.text
-    redis_instance.setex(url, 10, html_content)
-    return html_content
+    return response.text
 
 
 if __name__ == "__main__":
